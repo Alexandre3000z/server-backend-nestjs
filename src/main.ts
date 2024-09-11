1dc8629a6610eee14be3136911996d94af0906c5
Alexandre3000z <joaoalexandrems3000@gmail.com>
2024-09-11 13:28:27 -0300

    liberar ip

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173', // Endereço da sua aplicação React
+    origin: '*', // Endereço da sua aplicação React
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
